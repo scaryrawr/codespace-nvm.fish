b6702da765f000fcb7f3e9ba01db3b7612352cb4
Mike Wallio <michael.wallio@outlook.com>
2024-10-31 15:25:07 -0400

    Add completions

--- completions/nvm.fish
+++ completions/nvm.fish
@@ -4,18 +4,19 @@ complete --command nvm --exclusive --long help --description "Print help"
 complete --command nvm --long silent --description "Suppress standard output"
 
 complete --command nvm --exclusive --condition __fish_use_subcommand --arguments install --description "Download and activate the specified Node version"
+complete --command nvm --exclusive --condition __fish_use_subcommand --arguments alias --description "Set an alias named <name> pointing to <version>"
+complete --command nvm --exclusive --condition __fish_use_subcommand --arguments exec --description "Run <command> on <version>. Uses .nvmrc if available and version is omitted."
+complete --command nvm --exclusive --condition __fish_use_subcommand --arguments run --description "Run `node` on <version> with <args> as arguments. Uses .nvmrc if available and version is omitted."
 complete --command nvm --exclusive --condition __fish_use_subcommand --arguments use --description "Activate the specified Node version in the current shell"
 complete --command nvm --exclusive --condition __fish_use_subcommand --arguments list --description "List installed Node versions"
 complete --command nvm --exclusive --condition __fish_use_subcommand --arguments list-remote --description "List available Node versions to install"
+complete --command nvm --exclusive --condition __fish_use_subcommand --arguments ls --description "List installed Node versions"
+complete --command nvm --exclusive --condition __fish_use_subcommand --arguments ls-remote --description "List available Node versions to install"
 complete --command nvm --exclusive --condition __fish_use_subcommand --arguments current --description "Print the currently-active Node version"
-complete --command nvm --exclusive --condition "__fish_seen_subcommand_from install" --arguments "(
-    test -e $nvm_data && string split ' ' <$nvm_data/.index
-)"
-complete --command nvm --exclusive --condition "__fish_seen_subcommand_from use" --arguments "(_nvm_list | string split ' ')"
+complete --command nvm --exclusive --condition "__fish_seen_subcommand_from install" --arguments "(echo '--lts';echo '--latest';nvm ls-remote | grep -Po 'v\d+\.\d+\.\d+')"
+complete --command nvm --exclusive --condition "__fish_seen_subcommand_from use" --arguments "(ls $NVM_DIR/versions/node)"
 complete --command nvm --exclusive --condition __fish_use_subcommand --arguments uninstall --description "Uninstall the specified Node version"
-complete --command nvm --exclusive --condition "__fish_seen_subcommand_from uninstall" --arguments "(
-    _nvm_list | string split ' ' | string replace system ''
-)"
-complete --command nvm --exclusive --condition "__fish_seen_subcommand_from use uninstall" --arguments "(
-    set --query nvm_default_version && echo default
-)"
+complete --command nvm --exclusive --condition "__fish_seen_subcommand_from uninstall" --arguments "(ls $NVM_DIR/versions/node)"
+complete --command nvm --exclusive --condition "__fish_seen_subcommand_from alias" --arguments "(ls $NVM_DIR/versions/node)"
+complete --command nvm --exclusive --condition "__fish_seen_subcommand_from exec" --arguments "(ls $NVM_DIR/versions/node)"
+complete --command nvm --exclusive --condition "__fish_seen_subcommand_from run" --arguments "(ls $NVM_DIR/versions/node)"
